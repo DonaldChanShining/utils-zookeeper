86dcdd101b5a1342c5aa3214ed89cfd11fe10507
Yan Pujante <yan@pongasoft.com>
2015-05-06 08:42:46 -1000

    Upgraded (direct) dependencies to more recent versions
    gradle 2.3 (was 1.11)
    gradle-plugins: 2.2.8 (was 2.2.6)
    groovy: 2.4.3 (was 2.0.7)
    utils-misc: 2.1.0 (was 2.0.3)
    slf4j: 1.7.10 (was 1.6.2)
    junit: 4.12 (was 4.10)
    log4j: 1.2.17 (was 1.2.16)
    zookeeper: 3.4.6 (was 3.4.5)

--- project-spec.groovy
+++ project-spec.groovy
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2010-2010 LinkedIn, Inc
- * Portions Copyright (c) 2012-2013 Yan Pujante
+ * Portions Copyright (c) 2012-2015 Yan Pujante
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -18,13 +18,13 @@
 spec = [
   name: 'utils-zookeeper',
   group: 'org.pongasoft',
-  version: '2.0.3',
+  version: '2.1.0',
 
   versions: [
     jdk: '1.7',
-    groovy: '2.0.7',
-    utilsMisc: '2.0.3',
-    slf4j: '1.6.2' // to be compatible with grails 2.2.1
+    groovy: '2.4.3',
+    utilsMisc: '2.1.0',
+    slf4j: '1.7.10' // to be compatible with grails 2.5.0
   ],
 
   // information about the build framework itself
@@ -47,14 +47,14 @@ spec.external = [
   commonsCli: 'commons-cli:commons-cli:1.2',
   groovy: "org.codehaus.groovy:groovy-all:${spec.versions.groovy}",
   groovyTest: "org.codehaus.groovy:groovy-test:${spec.versions.groovy}",
-  junit: 'junit:junit:4.10',
+  junit: 'junit:junit:4.12',
   utilsMiscCore: "org.pongasoft:org.linkedin.util-core:${spec.versions.utilsMisc}",
   utilsMiscGroovy: "org.pongasoft:org.linkedin.util-groovy:${spec.versions.utilsMisc}",
-  log4j: 'log4j:log4j:1.2.16',
+  log4j: 'log4j:log4j:1.2.17',
   slf4j: "org.slf4j:slf4j-api:${spec.versions.slf4j}",
   slf4jLog4j: "org.slf4j:slf4j-log4j12:${spec.versions.slf4j}",
   slf4jJul: "org.slf4j:jul-to-slf4j:${spec.versions.slf4j}",
-  zookeeper: 'org.apache.zookeeper:zookeeper:3.4.5'
+  zookeeper: 'org.apache.zookeeper:zookeeper:3.4.6'
 ]
 
 // information about the bintray distribution
